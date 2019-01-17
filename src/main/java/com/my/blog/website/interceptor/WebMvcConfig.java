acd15a9f9814e5c0c555b55afd3af709615ad64d
V200123 <295799682@qq.com>
2019-01-17 17:52:49 +0800

    将版本改为2.0.1
    添加了druid的监控
    
    加入了登录页面的国际化

--- src/main/java/com/my/blog/website/interceptor/WebMvcConfig.java
+++ src/main/java/com/my/blog/website/interceptor/WebMvcConfig.java
@@ -1,21 +1,19 @@
 package com.my.blog.website.interceptor;
 
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
 import com.my.blog.website.utils.TaleUtils;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 /**
  * 向mvc中添加自定义组件
@@ -37,7 +35,28 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**").addResourceLocations("file:"+ TaleUtils.getUploadFilePath()+"upload/");
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new NativeLocaleResolver();
+    }
+    protected static class NativeLocaleResolver implements LocaleResolver {
 
+        @Override
+        public Locale resolveLocale(HttpServletRequest request) {
+            String language = request.getParameter("l");
+            Locale locale = Locale.getDefault();
+            if (!StringUtils.isEmpty(language)) {
+                String[] split = language.split("_");
+                locale = new Locale(split[0], split[1]);
+            }
+            return locale;
+        }
+
+        @Override
+        public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+        }
     }
 
 }
