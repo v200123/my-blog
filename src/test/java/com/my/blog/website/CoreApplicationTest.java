f698e4cd8533dda548133de665c57f0432041e84
V200123 <295799682@qq.com>
2019-01-17 18:05:18 +0800

    将版本改为2.0.1
    添加了druid的监控
    
    加入了登录页面的国际化

---
+++ src/test/java/com/my/blog/website/CoreApplicationTest.java
@@ -0,0 +1,21 @@
+package com.my.blog.website;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreApplicationTest
+{
+    private static final Logger logger = LoggerFactory.getLogger(CoreApplicationTest.class);
+    @Value("${spring.datasource.url}")
+    private String url;
+
+}
